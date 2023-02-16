396a4644725867520097fc0146b587d0176239c7
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2023-02-16 16:58:48 +0530

    Create UserUpdateDTO.java

---
+++ bookess/src/main/java/com/assignment/dto/UserUpdateDTO.java
@@ -0,0 +1,30 @@
+package com.assignment.dto;
+
+public class UserUpdateDTO {
+	
+	private String name;
+	private boolean isAdmin;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean getIsAdmin() {
+		return isAdmin;
+	}
+	public void setIsAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public UserUpdateDTO(String name, boolean isAdmin) {
+		super();
+		this.name = name;
+		this.isAdmin = isAdmin;
+	}
+	public UserUpdateDTO() {
+		super();
+	}
+
+}
