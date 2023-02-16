bb64aa2bf9f4e8343092e80a1ecb6264c1e8a645
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2023-02-16 17:02:16 +0530

    Create BookConstants.java

---
+++ bookess/src/main/java/com/assignment/utils/BookConstants.java
@@ -0,0 +1,8 @@
+package com.assignment.utils;
+
+public class BookConstants {
+	
+	public static final String DEFAULT_SORT_BY = "id";
+    public static final String DEFAULT_SORT_DIRECTION = "asc";
+
+}
