107a98a712557b0916f81cc4c76ffdce60acc07e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2023-02-16 17:00:34 +0530

    Create bookess.sql

---
+++ bookess.sql
@@ -0,0 +1,11 @@
+create database bookess;
+
+use bookess;
+
+show tables;
+
+select * from user;
+select * from book;
+select * from logout_token;
+select * from user_liked_books;
+select * from user_read_later_books;
