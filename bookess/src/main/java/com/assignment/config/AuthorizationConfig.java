92b308b25ef4d40b0eb2b7a2f4c8935d3379b992
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2023-02-16 17:01:47 +0530

    Create AuthorizationConfig.java

---
+++ bookess/src/main/java/com/assignment/config/AuthorizationConfig.java
@@ -0,0 +1,25 @@
+package com.assignment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.assignment.interceptor.AuthorizationInterceptor;
+
+@Configuration
+@EnableWebMvc
+public class AuthorizationConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public AuthorizationInterceptor authorizationInterceptor() {
+	    return new AuthorizationInterceptor();
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authorizationInterceptor()).addPathPatterns("/**");
+	}
+
+}
